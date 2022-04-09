f650c193ca8aedc5b735095d8beb1ab028bfe1f1
Rahul Sharma <rahul23shrm@gmail.com>
2022-04-10 03:23:47 +0530

    Heroku error fix

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const chats = require("./sampleData");
 const dotenv = require("dotenv");
-const path = require("path");
 const cors = require("cors");
 const connectToDB = require("./database");
 const userRoutes = require("./routes/userRoutes");
