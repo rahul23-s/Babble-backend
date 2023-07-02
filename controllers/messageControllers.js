d5335e630dca4ae65a6e9da34437003e69542525
Rahul <rahul23shrm@gmail.com>
2023-07-03 00:33:26 +0530

    Added Delete msg api

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -62,11 +62,11 @@ const deleteMessage = expressAsyncHandler(async (req, res) => {
       return res.status(404).json({ message: "Message not found" });
     }
 
-    res.json({ message: "Message deleted successfully" });
+    res.json({ message: "Message deleted successfully", data: deletedMessage });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
   }
-})
+});
 
 module.exports = { sendMessage, allMessages, deleteMessage };
